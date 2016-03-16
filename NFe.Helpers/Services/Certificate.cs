2337059cc5405560543b1291befb1da812d6a17e
andersonlemos <alemos@dmorj.com>
2016-03-16 17:14:27 -0300

    Remove unused usings

--- NFe.Helpers/Services/Certificate.cs
+++ NFe.Helpers/Services/Certificate.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace NFe.Helpers.Services
